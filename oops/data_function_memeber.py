56face2d6e78ec46090651a07e50fd7051ec04be
Puneeth2002 <oyyavuru.puneeth@gmail.com>
2023-07-10 17:26:32 +0530

    oops creation process

---
+++ oops/data_function_memeber.py
@@ -0,0 +1,14 @@
+class test:
+    a=0
+    b=0
+    def read(self):
+        test.a=eval(input("enter a value:"))
+        test.b=eval(input("enter b value:"))
+
+    def print(self):
+        print("A value is",test.a)
+        print("B value is",test.b)
+
+test_obj=test()
+test_obj.read()
+test_obj.print()
