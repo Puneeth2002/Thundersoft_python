a7d3c7bed0df72a6944af3e5254c852a03d0df16
Puneeth2002 <oyyavuru.puneeth@gmail.com>
2023-07-11 17:26:26 +0530

    constructor_overloading creation process

---
+++ constructor_overloading/into_withhex_function overloading.py
@@ -0,0 +1,18 @@
+class student:
+    count=0
+    def __init__(self,sno,sname,location):
+        student.count+=1
+        self.rno=sno
+        self.sname=sname
+        self.location=location
+    def display(self):
+        print("Roll Number is:",self.rno)
+        print("Name is:",self.sname)
+        print("Location is:",self.location)
+
+s1=student("1","sai","hyd")
+print("Address of s1",hex(id(s1)))
+s2=student("2","Ram","chennai")
+print("Address of s2",hex(id(s2)))      
+s1.display()
+s2.display()
